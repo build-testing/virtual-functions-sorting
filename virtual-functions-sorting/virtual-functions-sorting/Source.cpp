66f502fb773ad60c14105f5163d7e699df68e000
gmh5225 <2315157@qq.com>
2024-02-17 13:04:18 +0800

    1

--- virtual-functions-sorting/virtual-functions-sorting/Source.cpp
+++ virtual-functions-sorting/virtual-functions-sorting/Source.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+//#include <stdio.h>
 
 
 
@@ -6,22 +6,33 @@ class MyClass
 {
 public:
 	virtual void xx() = 0;
+	virtual void xx(double) = 0;
 	virtual void yy() = 0;
+	virtual void xx(float) = 0;
 	virtual void xx(int) = 0;
-	virtual void xx(int,int) = 0;
+	virtual void xx(int, int) = 0;
 };
 
 class MyClass2
 {
 public:
-	MyClass2() { printf("MyClass2\n"); }
+	MyClass2() { //printf("MyClass2\n"); 
+	}
 	~MyClass2() {}
 
 public:
-	virtual void xx() { printf("xx1\n"); }
-	virtual void yy() { printf("yy\n"); }
-	virtual void xx(int a) { printf("xx2=%d\n", a); }
-	virtual void xx(int a,int b) { printf("xx3=%d\n", a); }
+	virtual void xx() { //printf("xx1\n"); 
+	}
+	virtual void yy() { //printf("yy\n"); 
+	}
+	virtual void xx(int a) { //printf("xx2=%d\n", a); 
+	}
+	virtual void xx(int a, int b) { //printf("xx3=%d\n", a); 
+	}
+	virtual void xx(double a) { //printf("xx4=%f\n", a); 
+	}
+	virtual void xx(float a) { //printf("xx5=%f\n", a); 
+	}
 };
 
 
@@ -30,17 +41,15 @@ int main()
 
 	{
 		MyClass2* p = new MyClass2();
-		p->xx();//			MSVC-2015/2019: +10
-		p->xx(2);//			MSVC-2015/2019: +8
-		p->xx(4, 4);//		MSVC-2015/2019: +0
-		p->yy();//			MSVC-2015/2019: +18
+		p->xx();//						LLVM-MSVC/MSVC-2015/2019: +20
+		p->xx(2);//						LLVM-MSVC/MSVC-2015/2019: +18
+		p->xx(4, 4);//					LLVM-MSVC/MSVC-2015/2019: +10
+		p->yy();//						LLVM-MSVC/MSVC-2015/2019: +28
+		p->xx((double)0.2f);//			LLVM-MSVC/MSVC-2015/2019: +8
+		p->xx((float)0.4f);//			LLVM-MSVC/MSVC-2015/2019: +0
 	}
-	
+
 	return 0;
 }
-
-
-
-
 
 
